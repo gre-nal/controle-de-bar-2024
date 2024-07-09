9a91169c223792b92bcd2f033037489ee2b5c947
Matheus-Branco <jeremiasmatheus007@gmail.com>
2024-07-09 15:52:35 -0300

    continuação

--- ControleBar.Dominio/Compartilhado/EntidadeBase.cs
+++ ControleBar.Dominio/Compartilhado/EntidadeBase.cs
@@ -1,4 +1,4 @@
-﻿namespace ControleBar.Dominio;
+﻿namespace ControleBar.Dominio.Compartilhado;
 public abstract class EntidadeBase
 {
     public int Id { get; set; }
--- ControleBar.Infraestrutura.Orm/Mesa/RepositorioMesaOrm.cs
+++ ControleBar.Infraestrutura.Orm/Mesa/RepositorioMesaOrm.cs
@@ -3,10 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ControleBar.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.SqlServer;
 
 namespace ControleBar.Infraestrutura.Orm.Mesa
 {
-    internal class RepositorioMesaOrm
+    public class RepositorioMesaOrm : IRepositorioMesa
     {
+        public void Cadastrar(Mesa mesa)
+        {
+            dbContext.Mesas.Add(mesa);
+            dbContext.SaveChanges();
+        }
+        public bool Editar(int id, Mesa questaoAtualizada)
+        {
+            Mesa questao = dbContext.Mesas.Find(id)!;
+
+            if (mesa == null)
+                return false;
+
+            mesa.AtualizarRegistro(mesaAtualizada);
+
+            dbContext.Questoes.Update(mesa);
+            dbContext.SaveChanges();
+            return true;
+        }
+        public bool Excluir(int id)
+        {
+            Mesa mesa = dbContext.Mesas.Find(id)!;
+
+            if (mesa == null)
+                return false;
+
+            dbContext.Mesas.Remove(mesa);
+            dbContext.SaveChanges();
+
+            return true;
+        }
+
+        public Mesa SelecionarPorId(int id)
+        {
+            return dbContext.Mesas.Include(m => m.Numero).FirstOrDefault(m => m.Id == id)!;
+        }
+
+        public List<Mesa> SelecionarTodos()
+        {
+            return dbContext.Mesas.Include(m => m.Numero).ToList();
+        }
     }
 }
